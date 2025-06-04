20ea52a1e819a966f7741abea3e8d7e6cbe2e41f
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-04 19:00:20 -0400

    Creacion TareaDao

---
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p1/data/local/Dao/TareaDao.kt
@@ -0,0 +1,30 @@
+package edu.ucne.wilmercastillo_ap2_p1.data.local.Dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.wilmercastillo_ap2_p1.data.local.Entity.TareaEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface  TareaDao{
+    @Upsert()
+    suspend fun save(tarea: TareaEntity)
+    @Query(
+        """
+    SELECT *
+    FROM Tareas
+    WHERE  tareaId=:id
+    LIMIT 1
+    """
+    )
+    suspend fun find(id : Int): TareaEntity?
+    @Delete
+    suspend fun delete(tarea: TareaEntity)
+    @Query("SELECT * FROM Tareas")
+    fun getAll(): Flow<List<TareaEntity>>
+}
+
+
