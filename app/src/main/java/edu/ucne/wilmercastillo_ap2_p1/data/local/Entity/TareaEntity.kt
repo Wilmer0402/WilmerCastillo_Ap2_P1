17d415bd3c810045c5148c485165f0107e550c4f
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-04 19:01:17 -0400

    Creacion TareaEntity

---
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p1/data/local/Entity/TareaEntity.kt
@@ -0,0 +1,13 @@
+package edu.ucne.wilmercastillo_ap2_p1.data.local.Entity
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="Tareas")
+data class TareaEntity(
+    @PrimaryKey
+    val tareaId: Int? = 0,
+    val descripcion: String = "",
+    val minutos: Int? = 0
+)
